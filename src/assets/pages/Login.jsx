0e7173d87b2bb4264075d6b7488afe247cd6d3f3
lesajayaweera <lesanduluthmeen8@gmail.com>
2025-07-23 12:23:31 +0530

    started to do the Login

--- src/assets/pages/Login.jsx
+++ src/assets/pages/Login.jsx
@@ -1,7 +1,14 @@
-function Login(){
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+function Login({isAuthenticated}){
+    const navigate = useNavigate();
+
+    const [username, SetUsername] = useState('');
+    const [password,SetPassword]= useState('');
     return(
         <div>
-            <p>Hello world</p>
+            
         </div>
     );
 
